599ffaa0694bf5e0884e51d7489c243e2a90ecb6
John Harrigan <jharriga@redhat.com>
2020-09-22 14:41:33 -0400

    Create cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# CLEANUP.sh
+# User must create $CFGFILE proir to running this script
+# See   "s3cmd --configure -c s3test.cfg"
+
+CFGFILE=./s3test.cfg
+# make sure S3 cfg file exists
+[ -f "$CFGFILE" ] && echo "continuing" || echo "$CFGFILE does not exist."; exit
+
+# Empty and Remove the buckets 
+s3cmd rb s3://mycontainers1 -c s3test.cfg --recursive || {
+    echo "Error removing s3://mycontainers1, exit"
+    exit 1
+}
+s3cmd rb s3://mycontainers2 -c s3test.cfg --recursive || {
+    echo "Error removing s3://mycontainers2, exit"
+    exit 1
+}
+s3cmd rb s3://mycontainers3 -c s3test.cfg --recursive || {
+    echo "Error removing s3://mycontainers3, exit"
+    exit 1
+}
+s3cmd rb s3://mycontainers4 -c s3test.cfg --recursive || {
+    echo "Error removing s3://mycontainers4, exit"
+    exit 1
+}
+s3cmd rb s3://mycontainers5 -c s3test.cfg --recursive || {
+    echo "Error removing s3://mycontainers5, exit"
+    exit 1
+}
+
+# Process GCs
+radosgw-admin gc process gc --include-all &
+radosgw-admin gc process gc --include-all &
+radosgw-admin gc process gc --include-all &
+
+# Wait for all GCs to be processed, none PENDING
+Utils/completedGC.sh 5m /tmp/hold
+
+# cleanup
+rm -f /tmp/hold
